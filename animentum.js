6354e09256f6eab3694a25be07ca5f48f6f1976c
0lbap <pablo.jovelaviron@orange.fr>
2021-04-14 10:15:51 +0200

    Update animentum.js

--- animentum.js
+++ animentum.js
@@ -1,1 +1,1 @@
-document.body.style.border = "5px solid red";
+document.getElementsByClassName("visuel_ent_um")[0].getElementsByTagName("IMG")[0].src="https://sh3ff1e.files.wordpress.com/2011/08/cropped-wallpaper1171856.jpg";

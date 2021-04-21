1801b649152298dc4c5220f50ccf2a2c00375c3c
0lbap <pablo.jovelaviron@orange.fr>
2021-04-21 22:32:40 +0200

    Update animentum.js

--- animentum.js
+++ animentum.js
@@ -1,1 +1,10 @@
-document.getElementsByClassName("visuel_ent_um")[0].getElementsByTagName("IMG")[0].src="https://sh3ff1e.files.wordpress.com/2011/08/cropped-wallpaper1171856.jpg";
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+var url="https://www.assets.hawkbydesign.com/randomanime/images/anime/"+getRandomInt(1556)+"/anime-l.jpg";
+bannerdiv = document.getElementsByClassName("visuel_ent_um")[0]
+bannerimg = bannerdiv.getElementsByTagName("IMG")[0]
+bannerimg.src=url;
+bannerimg.style.height="auto";
+bannerimg.style.maxHeight="none";
+bannerdiv.style.height=280;
